dfdf5f69dbb90803cbe17c2ad51a0f4d16459a59
William Yeung <williamyeung1141998@gmail.com>
2020-11-16 14:59:26 -0500

    First commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by William Yeung on 11/16/20.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
