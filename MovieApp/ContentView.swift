794ccfb0d3e9ee1989674283e0a89c35bd0a3f3e
William Yeung <williamyeung1141998@gmail.com>
2020-11-20 20:30:43 -0500

    using SceneStorage to save last tab a user was on

--- MovieApp/ContentView.swift
+++ MovieApp/ContentView.swift
@@ -8,28 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @SceneStorage("selectedView") var selectedView: String?
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selectedView) {
             HomeView()
+                .tag(HomeView.tag)
                 .tabItem {
                     Image(systemName: "house")
                     Text("Home")
                 }
             
             ProjectView(showClosedProjects: false)
+                .tag(ProjectView.openTag)
                 .tabItem {
                     Image(systemName: "list.bullet")
                     Text("Open")
                 }
             
             ProjectView(showClosedProjects: true)
+                .tag(ProjectView.closedTag)
                 .tabItem {
                     Image(systemName: "checkmark")
                     Text("Closed")
                 }
-        }
-        .onAppear() {
-            print("PRINT THIS: \(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))")
         }
     }
 }
