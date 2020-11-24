f35b460c2b3cf8a36ac03f8e196047df368b6cab
William Yeung <williamyeung1141998@gmail.com>
2020-11-24 12:31:07 -0500

    Creating editItemView and allowing basic data editing and displaying new Item information for a project

---
+++ MovieApp/ItemRowView.swift
@@ -0,0 +1,24 @@
+//
+//  ItemRowView.swift
+//  MovieApp
+//
+//  Created by William Yeung on 11/24/20.
+//
+
+import SwiftUI
+
+struct ItemRowView: View {
+    @ObservedObject var item: Item
+    
+    var body: some View {
+        NavigationLink(destination: EditItemView(item: item)) {
+            Text(item.itemTitle)
+        }
+    }
+}
+
+struct ItemRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRowView(item: Item.example)
+    }
+}
